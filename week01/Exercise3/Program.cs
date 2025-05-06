c732bdebc0ecce8095e618917d82707e2177defa
jneberhard <jreberhard3@gmail.com>
2025-05-05 19:38:32 -0600

    WK1 ex3

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -1,9 +1,35 @@
 using System;
+using System.Net;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
+        //int magic = -2;
+        //Console.Write("What is the magic number? ");
+        // magic = int.Parse(Console.ReadLine());
+        Random randomGenerator = new Random();
+        int magic = randomGenerator.Next(1, 100);
+        int guess = -1;
+
+        while (guess != magic)
+        {
+            Console.Write("What is your guess? ");
+            guess = int.Parse(Console.ReadLine());
+
+            if (magic > guess)
+            {
+                Console.WriteLine("Higher");
+            }
+            else if (magic < guess)
+            {
+                Console.WriteLine("Lower");
+            }
+            else
+            {
+                Console.WriteLine("You got it");
+            }
+
+        }
     }
 }
